508b1d37ad51c4d8053743be9e944a63bc6365c3
lyramer <andy.wynden@gmail.com>
2021-10-23 19:01:48 -0700

    cleaning up detritus

--- src/Components/DataSources/wms.js
+++ src/Components/DataSources/wms.js
@@ -2,10 +2,6 @@ import * as olSource from "ol/source";
 
 function wms(props) {
     const {url, params, serverType} = {...props}
-    console.log("generating wms request source for...")
-    console.log("url: ", url)
-    console.log("params: ", params)
-    console.log("serverType: ", serverType)
     return new olSource.TileWMS({
         url,
         params,
